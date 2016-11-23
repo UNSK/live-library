9d72b64ccfababee450ebcd9b628073806c6adec
UNSK <werdna.b10@gmail.com>
2016-11-23 16:31:20 +0900

    雑に追加してみた

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120085953) do
-
+ActiveRecord::Schema.define(version: 20_161_120_085_953) do
   create_table "books", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
@@ -24,5 +24,4 @@ ActiveRecord::Schema.define(version: 20161120085953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
